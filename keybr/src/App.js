977b505a9bc382cc845c06c0d9ee9a6b4298d589
ManuelJF <skaflon_war@hotmail.com>
2017-06-26 15:27:43 -0500

    adding 4 lessons and a lot of excercises

--- keybr/src/App.js
+++ keybr/src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
     this.onKey = this.onKey.bind(this);
     this.onClose = this.onClose.bind(this);
     this.state = {
-      strgeneralIndex: '',
-      // level: 0,
       acurate: 0,
       hits: 0,
       errors: 0,
@@ -20,23 +18,15 @@ class App extends Component {
       index: 0,
       generalIndex: 0,
       colorCorrect: { color: 'green'},
-      status: "",
       acurateColor: 'red',
       correctChar: '',
       modal: '',
-      iniIndex: 0,
-      finIndex: 61,
       excerciseNumber: 0,
-      // lessons: [
-      //   ["asdfg hjkl; asdfg hjkl ;lkjh gfdsa ;lkjh gfdsa","ah had lag slag ah had lag slag ah had","hash flash ask has hash flash ask has hash","dash gash lash dash gash lash dash gash lash","lad sash flag lad sash flag lad sash flag","fall gall hall lass fall gall hall lass fall","glass alfalfa adds glass alfalfa adds glass alfalfa adds","sad shall salad sad shall salad sad shall salad","ash glad alas ash glad alas ash glad alas","all flask half all flask half all flask half"],
-      //   ['qwert yuiop qwert yuiop ; poiuy trewq poiuy trewq','till quail tight yell; will those feat lake hash;','wire this flay queue; there rail sale gypsy is;','usher wake yaw tide; quash their light rash that;','slit guide tale rage; the depth quest your these;','tale trash slide day; take urge wages wake typist;','play rest jaw quail; why fell its well peat two;','typed sure peep sill; ogre rate quell paw yap use;','if do pit dug wary; quaff law stop lair gate here;','go her with hit; fight tug quill day saw yippee;'],
-      //   ['zxcvb nm,./ zxcvb nm,./ /.,mn bvcxz /.,mn bvcxz ','by, zip wean trying stump shrink rice ping/ mail ','king fan crick, chill block. back abbot. aflame/ mix ','zest win, thump skunk. plain muffin, knight/ exit ','daring. choke bishop. admit twine sultan, sing/ roman ','pink, nicest mate knit. groan flock, divide coop/ advice ','vilest turbot, track snick scoot revoke. rein/ paving ','Office. Meat. Lean. Hiking. Feline. Decay. Clash. Bright. ','Bump. Prank. Bate. Turnip. Bap. Aging. Aback. Wombat. Vine. ','Vexing. Typing. Being. Sump. Faxing. Rink. Blot. Nice. '],
-      //   ['12345 67890 12345 67890 09876 54321 09876 54321', '380, 157, 062, 519.238, 406, 925, 640, 502.798, 256', 'A1 S2 D3 F4 G5 H6 J7 K8 L9 ;0 9l 8k 7j 6h 5g 4f 3d 2s 1a', 'Moscow 013 Sofia 982 Nuuk 176 Riga 541 Vilnius 194', '32A Rome 174B Paris 985 Berlin 681 Madrid 704 Vienna', '831 Warsaw 495 Amsterdam 672 Brussels 140 Copenhagen', '640 Prague 135 Bratislava 217 Nicosia 489 Tallinn 810', '7.72 Helsinki 6.39 Athens 7.02 Valletta 14.2 Lisbon 87.5', '16.9 Ljubljana 0.24 Stockholm 81.3 Dublin 5.39 Oslo 71.6', '76.9 Belgrade 1.35 Reykjavik 72.0 Bern 86.4 Kiev' ]
-      // ],
       lessons: [
-        ["hello world","hello python"],
-        ["hello javascript","hello C++"],
-        ["its working its working"]
+        ["asdfg hjkl; asdfg hjkl ;lkjh gfdsa ;lkjh gfdsa","ah had lag slag ah had lag slag ah had","hash flash ask has hash flash ask has hash","dash gash lash dash gash lash dash gash lash","lad sash flag lad sash flag lad sash flag","fall gall hall lass fall gall hall lass fall","glass alfalfa adds glass alfalfa adds glass alfalfa adds","sad shall salad sad shall salad sad shall salad","ash glad alas ash glad alas ash glad alas","all flask half all flask half all flask half"],
+        ['qwert yuiop qwert yuiop ; poiuy trewq poiuy trewq','till quail tight yell; will those feat lake hash;','wire this flay queue; there rail sale gypsy is;','usher wake yaw tide; quash their light rash that;','slit guide tale rage; the depth quest your these;','tale trash slide day; take urge wages wake typist;','play rest jaw quail; why fell its well peat two;','typed sure peep sill; ogre rate quell paw yap use;','if do pit dug wary; quaff law stop lair gate here;','go her with hit; fight tug quill day saw yippee;'],
+        ['zxcvb nm,./ zxcvb nm,./ /.,mn bvcxz /.,mn bvcxz ','by, zip wean trying stump shrink rice ping/ mail ','king fan crick, chill block. back abbot. aflame/ mix ','zest win, thump skunk. plain muffin, knight/ exit ','daring. choke bishop. admit twine sultan, sing/ roman ','pink, nicest mate knit. groan flock, divide coop/ advice ','vilest turbot, track snick scoot revoke. rein/ paving ','Office. Meat. Lean. Hiking. Feline. Decay. Clash. Bright. ','Bump. Prank. Bate. Turnip. Bap. Aging. Aback. Wombat. Vine. ','Vexing. Typing. Being. Sump. Faxing. Rink. Blot. Nice. '],
+        ['12345 67890 12345 67890 09876 54321 09876 54321', '380, 157, 062, 519.238, 406, 925, 640, 502.798, 256', 'A1 S2 D3 F4 G5 H6 J7 K8 L9 ;0 9l 8k 7j 6h 5g 4f 3d 2s 1a', 'Moscow 013 Sofia 982 Nuuk 176 Riga 541 Vilnius 194', '32A Rome 174B Paris 985 Berlin 681 Madrid 704 Vienna', '831 Warsaw 495 Amsterdam 672 Brussels 140 Copenhagen', '640 Prague 135 Bratislava 217 Nicosia 489 Tallinn 810', '7.72 Helsinki 6.39 Athens 7.02 Valletta 14.2 Lisbon 87.5', '16.9 Ljubljana 0.24 Stockholm 81.3 Dublin 5.39 Oslo 71.6', '76.9 Belgrade 1.35 Reykjavik 72.0 Bern 86.4 Kiev' ]
       ],
       lesson: 0
     }
@@ -126,24 +116,10 @@ class App extends Component {
     }
     if (generalIndex === lessons[lesson][excerciseNumber].length){
       document.removeEventListener("keypress",this.onKey)
-      // var level = this.state.level
-      // level++
       excerciseNumber++
-      // var date = new Date();
-      // var local = this.state.localStorage
-      // var obj = {
-      //   'date': date,
-      //   'level': level,
-      //   'hits': this.state.hits,
-      //   'errors': this.state.errors,
-      //   'acurate': this.state.acurate,
-      //   'excercise': this.state.excercises[excerciseNumber]
-      // }
-      // local.push(obj)
       let ejercicio = (excerciseNumber === lessons[lesson].length)? 0 : excerciseNumber
       this.setState({
         modal: <Modal hits={this.state.hits} errors={this.state.errors} acurate={this.state.acurate} close={this.onClose}/>,
-        // level: (level === this.state.lessons[lesson].length)? 0 : level,
         hits: 0,
         errors: 0,
         lastError: 0,
@@ -153,31 +129,20 @@ class App extends Component {
         correctChar: '',
         excerciseNumber: ejercicio
       })
-      console.log(ejercicio);
-      if (excerciseNumber === lessons[lesson].length) {
-        if (this.state.lesson !== this.state.lessons.length) {
-          lesson++
-          this.setState({
-            lesson: lesson
-          })
+    }
+    if (excerciseNumber === lessons[lesson].length && this.state.lesson !== this.state.lessons.length) {
+        this.state.lesson++
+        if (this.state.lesson === this.state.lessons.length) {
+          this.state.lesson = 0
         }
-        else {
-          lesson = lesson
-          this.setState({
-            lesson: lesson
-          })
-        }
-      }
-      else {
         this.setState({
-          lesson: lesson
+          lesson: this.state.lesson
         })
-      }
-      console.log('lesson',lesson)
-      console.log('excercises of the lesson',lessons[lesson].length)
-      console.log('excercise',this.state.excerciseNumber)
-      console.log('actually',lessons[lesson][this.state.excerciseNumber])
-      console.log('LECCIONES>>>>',this.state.lessons);
+    }
+    else {
+      this.setState({
+        lesson: (this.state.lesson === this.state.lessons.length)? 0 : lesson
+      })
     }
   }
   onClose(){
@@ -186,19 +151,10 @@ class App extends Component {
     })
     document.addEventListener("keypress",this.onKey)
   }
-  componentWillMount(){
-    console.log('lesson',this.state.lesson)
-    console.log('excercises of the lesson',this.state.lessons[this.state.lesson].length)
-    console.log('excercise',this.state.excerciseNumber)
-    console.log('actually',this.state.lessons[this.state.lesson][this.state.excerciseNumber])
-    console.log('LECCIONES>>>>',this.state.lessons);
-  }
   render() {
-    // console.log(this.state.localStorage);
     let excerciseNumber = this.state.excerciseNumber
-    // let lessons = this.state.lessons
+    let lessons = this.state.lessons
     let lesson = this.state.lesson
-    console.log('excercise NUMBER>>>>>>>>>',excerciseNumber)
     return (
       <div className="App">
         {this.state.modal}
